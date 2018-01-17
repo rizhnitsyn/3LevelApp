21cc37f78eacd24b1d1fd1e7e7226112c1bd1197
rizhnitsyn <enotius@tut.by>
2018-01-17 23:55:43 +0300

    third feature part 2

--- service/src/main/java/by/demo/MyService.java
+++ service/src/main/java/by/demo/MyService.java
@@ -5,6 +5,7 @@ public class MyService {
     public static String getInfoFromServiceLayer() {
         System.out.println("begin new feature");
         System.out.println("third feature");
+        System.out.println("third feature1");
         return MyDatabase.getInfoFromDbLayer() + " service layer. Cherry-pick/ Second test. Second Feature!";
 
     }
