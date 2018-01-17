52177ae25498cfd8ac84b63ec2487a5c9b65d4a4
rizhnitsyn <enotius@tut.by>
2018-01-17 23:51:40 +0300

    new feature

--- service/src/main/java/by/demo/MyService.java
+++ service/src/main/java/by/demo/MyService.java
@@ -3,6 +3,7 @@ package by.demo;
 public class MyService {
 
     public static String getInfoFromServiceLayer() {
+        System.out.println("begin new feature");
         return MyDatabase.getInfoFromDbLayer() + " service layer. Cherry-pick/ Second test. Second Feature!";
 
     }
