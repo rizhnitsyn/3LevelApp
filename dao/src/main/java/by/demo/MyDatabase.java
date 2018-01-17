50ac70ff0739831f3b20cd586889d2beb6e9c152
rizhnitsyn <Wegawega2>
2018-01-17 15:58:05 +0300

    finishing feature

--- dao/src/main/java/by/demo/MyDatabase.java
+++ dao/src/main/java/by/demo/MyDatabase.java
@@ -4,6 +4,6 @@ public class MyDatabase {
 
 
     public static String getInfoFromDbLayer() {
-        return "info from database! + adding new features";
+        return "info from database! + adding new features. One more feature";
     }
 }
