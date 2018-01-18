088431b2382c66732d91b4e2c388cb69728911d0
rizhnitsyn <Wegawega2>
2018-01-18 13:22:29 +0300

    feature 12

--- service/src/main/java/by/demo/MyService.java
+++ service/src/main/java/by/demo/MyService.java
@@ -3,7 +3,7 @@ package by.demo;
 public class MyService {
 
     public static String getInfoFromServiceLayer() {
-        System.out.println("begin new featureasdasd12312312");
+        System.out.println("begin new featureasdasd1231231212123");
         System.out.println("third fesdadaturesdssdfdf123");
         System.out.println("third featudfdfre1123");
         System.out.println("four feature1featursdsde1sdfdf");
