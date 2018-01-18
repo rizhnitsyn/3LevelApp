20a409ac763b930e94b2958a2d112dd4f1015799
rizhnitsyn <Wegawega2>
2018-01-18 13:25:41 +0300

    release 1.1

--- service/src/main/java/by/demo/MyService.java
+++ service/src/main/java/by/demo/MyService.java
@@ -6,7 +6,7 @@ public class MyService {
         System.out.println("begin new featureasdasd12312312");
         System.out.println("third fesdadaturesdssdfdf123");
         System.out.println("third featudfdfre1123");
-        System.out.println("four feature1featursdsde1sdfdf");
+        System.out.println("four feature1featursdsde1sdfdf12");
         System.out.println("four feature1feafdfdsdture1sdsd12312");
         System.out.println("four lastsdsdsdfdfsdfdf123");
         return MyDatabase.getInfoFromDbLayer() + " service layer. Cherry-pick/ Second test. Second Feature!qweqweqwe!!";
