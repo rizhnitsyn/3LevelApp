4031389137d66e44aa7601d4069806939e261080
rizhnitsyn <Wegawega2>
2018-01-18 10:05:12 +0300

    feature

--- service/src/main/java/by/demo/MyService.java
+++ service/src/main/java/by/demo/MyService.java
@@ -7,7 +7,7 @@ public class MyService {
         System.out.println("third fesdadaturesds");
         System.out.println("third featudfdfre1");
         System.out.println("four feature1featursdsde1");
-        System.out.println("four feature1feadsdture1sdsd");
+        System.out.println("four feature1feafdfdsdture1sdsd");
         System.out.println("four lastsdsdsdfdf");
         return MyDatabase.getInfoFromDbLayer() + " service layer. Cherry-pick/ Second test. Second Feature!qweqweqwe";
 
