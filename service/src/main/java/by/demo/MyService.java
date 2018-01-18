158aa3242927864d53a77c026bb23897f391fa78
rizhnitsyn <Wegawega2>
2018-01-18 13:20:07 +0300

    feature 9

--- service/src/main/java/by/demo/MyService.java
+++ service/src/main/java/by/demo/MyService.java
@@ -7,7 +7,7 @@ public class MyService {
         System.out.println("third fesdadaturesdssdfdf123");
         System.out.println("third featudfdfre1123");
         System.out.println("four feature1featursdsde1sdfdf");
-        System.out.println("four feature1feafdfdsdture1sdsd123");
+        System.out.println("four feature1feafdfdsdture1sdsd12312");
         System.out.println("four lastsdsdsdfdfsdfdf");
         return MyDatabase.getInfoFromDbLayer() + " service layer. Cherry-pick/ Second test. Second Feature!qweqweqwe";
 
