2b7a7dde8aca4aa2cf14f12bc1931fb410f3ffb6
rizhnitsyn <Wegawega2>
2018-01-18 10:04:54 +0300

    develop

--- service/src/main/java/by/demo/MyService.java
+++ service/src/main/java/by/demo/MyService.java
@@ -5,7 +5,7 @@ public class MyService {
     public static String getInfoFromServiceLayer() {
         System.out.println("begin new feature");
         System.out.println("third fesdadaturesds");
-        System.out.println("third feature1");
+        System.out.println("third featudfdfre1");
         System.out.println("four feature1featursdsde1");
         System.out.println("four feature1feadsdture1sdsd");
         System.out.println("four lastsdsdsdfdf");
